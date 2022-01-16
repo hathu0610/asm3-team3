ba9d3ccada48ab559721423dd0eeaf19f363f1dd
Thu Nguyen <hathu0610@gmail.com>
2022-01-16 21:20:24 +0700

    fix minor issues

--- app/js/script.js
+++ app/js/script.js
@@ -13,7 +13,29 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
-$(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header').css({'padding-right':scrollWidth});
-  }).resize();
+
+
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+  btnHamburger.addEventListener('click', function() {
+    console.log('open');
+    if(header.classList.contains('open')){
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');    
+        });
+    } else {
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    console.log(fadeElems);
+    fadeElems.forEach(function(element){
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');    
+    });
+    }
+});
